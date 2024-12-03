986bb2a90f5bc1303b3930e364b53680436f3efd
AimbotParce <marcparcerisa@gmail.com>
2024-12-04 00:57:57 +0100

    Load world at the start and broadcast messages

--- api/endpoints.py
+++ api/endpoints.py
@@ -11,12 +11,18 @@ def dm_connect():
     if password != app.config["DM_PASSWORD"]:
         disconnect()
     else:
-        emit("connected", namespace="/dm")
+        emit("connected", {"message": "Admin Connected"}, to=request.sid)
 
 
 @socketio.on("connect", namespace="/display")
 def display_connect():
-    emit("connected", namespace="/display")
+    emit("connected", {"message": "Display Connected"}, to=request.sid)
+    if app.config["WORLD"] is not None:
+        world = loadWorld()
+        region_id = world["current_region"]
+        emit("change_world", world, to=request.sid)
+        emit("change_region", loadVisibleRegion(region_id), to=request.sid)
+        emit("update_creatures", loadVisibleCreatures(region_id), to=request.sid)
 
 
 @socketio.on("disconnect", namespace="/dm")
@@ -37,43 +43,51 @@ def change_world(world_id: str):
     world = loadWorld()
     region_id = world["current_region"]
     emit("change_world", world, broadcast=True)
-    emit("change_region", loadVisibleRegion(region_id), namespace="/display")
-    emit("update_creatures", loadVisibleCreatures(region_id), namespace="/display")
+    emit("change_region", loadVisibleRegion(region_id), namespace="/display", broadcast=True)
+    emit("update_creatures", loadVisibleCreatures(region_id), namespace="/display", broadcast=True)
 
 
 @socketio.on("change_region", namespace="/dm")
 def change_region(region_id: str):
+    if app.config["WORLD"] is None:
+        return "No world selected", 400
     if not request.headers.get("Authorization") == app.config["DM_PASSWORD"]:
         disconnect()
     world = loadWorld()
     world["current_region"] = region_id
     updateWorld(world)
-    emit("change_region", loadVisibleRegion(region_id), namespace="/display")
-    emit("update_creatures", loadVisibleCreatures(region_id), namespace="/display")
+    emit("change_region", loadVisibleRegion(region_id), namespace="/display", broadcast=True)
+    emit("update_creatures", loadVisibleCreatures(region_id), namespace="/display", broadcast=True)
 
 
 @socketio.on("update_region", namespace="/dm")
 def update_region(region: Region):
+    if app.config["WORLD"] is None:
+        return "No world selected", 400
     if not request.headers.get("Authorization") == app.config["DM_PASSWORD"]:
         disconnect()
     updateRegion(region)
     region_id = region["id"]
     world = loadWorld()
     if world["current_region"] == region_id:
-        emit("update_region", loadVisibleRegion(region_id), namespace="/display")
+        emit("update_region", loadVisibleRegion(region_id), namespace="/display", broadcast=True)
 
 
 @socketio.on("update_creature", namespace="/dm")
 def update_creature(creature: Creature):
+    if app.config["WORLD"] is None:
+        return "No world selected", 400
     if not request.headers.get("Authorization") == app.config["DM_PASSWORD"]:
         disconnect()
     updateCreature(creature)
     region_id = creature["current_region"]
     world = loadWorld()
     if world["current_region"] == region_id:
-        emit("update_creatures", loadVisibleCreatures(region_id), namespace="/display")
+        emit("update_creatures", loadVisibleCreatures(region_id), namespace="/display", broadcast=True)
 
 
 @app.get("/images/<path:path>")
 def send_image(path):
+    if app.config["WORLD"] is None:
+        return "No world selected", 400
     return app.send_static_file(pathlib.Path(app.config["WORLD"]) / "images" / path)
