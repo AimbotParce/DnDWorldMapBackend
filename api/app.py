6678f3c7df5805760d40040826393bc1d6eb9fc5
AimbotParce <marcparcerisa@gmail.com>
2024-12-04 16:56:55 +0100

    Add the cors

--- api/app.py
+++ api/app.py
@@ -1,10 +1,12 @@
 import secrets
 
 import flask
+from flask_cors import CORS
 from flask_socketio import SocketIO
 
 app = flask.Flask(__name__)
 app.config["SECRET_KEY"] = secrets.token_urlsafe(16)
+CORS(app)
 socketio = SocketIO(app)
 
 app.config["DM_PASSWORD"] = open("admin.key").read().strip()
